44c12572a5d545909162fbfbe56686b2e25a9b1f
g1n0st <g1n0st@live.com>
2024-07-10 15:05:18 -0700

    [update] delete UpdateParticlesVelocityStateOnly

--- multibody/mpm/mpm_transfer.h
+++ multibody/mpm/mpm_transfer.h
@@ -65,9 +65,6 @@ class MpmTransfer {
    */
   void UpdateParticlesState(const ParticlesData<T>& particles_data, double dt,
                             Particles<T>* particles) const;
-  
-  void UpdateParticlesVelocityStateOnly(const ParticlesData<T>& particles_data, double dt,
-                            Particles<T>* particles) const;
 
   /**
    * Computes the grid_forces at all active grid nodes.
