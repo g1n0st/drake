9795c9767a9ea74540e0cefaf66b2a90e70fcda8
g1n0st <g1n0st@live.com>
2024-08-06 13:05:15 -0700

    [add] add const accessor methods

--- multibody/gpu_mpm/cuda_mpm_model.cuh
+++ multibody/gpu_mpm/cuda_mpm_model.cuh
@@ -26,16 +26,26 @@ public:
     const uint32_t& current_particle_buffer_id() const { return current_particle_buffer_id_; }
 
     T* current_positions() { return particle_buffer_[current_particle_buffer_id_].d_positions; }
+    const T* current_positions() const { return particle_buffer_[current_particle_buffer_id_].d_positions; }
     T* current_velocities() { return particle_buffer_[current_particle_buffer_id_].d_velocities; }
+    const T* current_velocities() const { return particle_buffer_[current_particle_buffer_id_].d_velocities; }
     T* current_volumes() { return particle_buffer_[current_particle_buffer_id_].d_volumes; }
+    const T* current_volumes() const { return particle_buffer_[current_particle_buffer_id_].d_volumes; }
     T* current_affine_matrices() { return particle_buffer_[current_particle_buffer_id_].d_affine_matrices; }
+    const T* current_affine_matrices() const { return particle_buffer_[current_particle_buffer_id_].d_affine_matrices; }
 
     T* forces() { return d_forces_; }
+    const T* forces() const { return d_forces_; }
     T* taus() { return d_taus_; }
+    const T* taus() const { return d_taus_; }
     T* deformation_gradients() { return d_deformation_gradients_; }
+    const T* deformation_gradients() const { return d_deformation_gradients_; }
     T* Dm_inverses() { return d_Dm_inverses_; }
+    const T* Dm_inverses() const { return d_Dm_inverses_; }
     int* indices() { return d_indices_; }
+    const int* indices() const { return d_indices_; }
     int* index_mappings() { return d_index_mappings_; }
+    const int* index_mappings() const { return d_index_mappings_; }
 
     T* next_positions() { return particle_buffer_[current_particle_buffer_id_ ^ 1].d_positions; }
     T* next_velocities() { return particle_buffer_[current_particle_buffer_id_ ^ 1].d_velocities; }
@@ -43,18 +53,27 @@ public:
     T* next_affine_matrices() { return particle_buffer_[current_particle_buffer_id_ ^ 1].d_affine_matrices; }
     
     int* current_pids() { return particle_buffer_[current_particle_buffer_id_].d_pids; }
+    const int* current_pids() const { return particle_buffer_[current_particle_buffer_id_].d_pids; }
     uint32_t* current_sort_keys() { return particle_buffer_[current_particle_buffer_id_].d_sort_keys; }
+    const uint32_t* current_sort_keys() const { return particle_buffer_[current_particle_buffer_id_].d_sort_keys; }
     uint32_t* current_sort_ids() { return particle_buffer_[current_particle_buffer_id_].d_sort_ids; }
+    const uint32_t* current_sort_ids() const { return particle_buffer_[current_particle_buffer_id_].d_sort_ids; }
+
     int* next_pids() { return particle_buffer_[current_particle_buffer_id_ ^ 1].d_pids; }
     uint32_t* next_sort_keys() { return particle_buffer_[current_particle_buffer_id_ ^ 1].d_sort_keys; }
     uint32_t* next_sort_ids() { return particle_buffer_[current_particle_buffer_id_ ^ 1].d_sort_ids; }
 
     T* grid_masses() { return grid_buffer_.d_g_masses; }
+    const T* grid_masses() const { return grid_buffer_.d_g_masses; }
     T* grid_momentum() { return grid_buffer_.d_g_momentum; }
+    const T* grid_momentum() const { return grid_buffer_.d_g_momentum; }
     uint32_t* grid_touched_flags() { return grid_buffer_.d_g_touched_flags; }
+    const uint32_t* grid_touched_flags() const { return grid_buffer_.d_g_touched_flags; }
     uint32_t* grid_touched_ids() { return grid_buffer_.d_g_touched_ids; }
+    const uint32_t* grid_touched_ids() const { return grid_buffer_.d_g_touched_ids; }
     uint32_t* grid_touched_cnt() { return grid_buffer_.d_g_touched_cnt; }
-    uint32_t grid_touched_cnt_host() {
+    const uint32_t* grid_touched_cnt() const { return grid_buffer_.d_g_touched_cnt; }
+    uint32_t grid_touched_cnt_host() const {
         uint32_t h_g_touched_cnt = 0u;
         CUDA_SAFE_CALL(cudaDeviceSynchronize());
         CUDA_SAFE_CALL(cudaMemcpy(&h_g_touched_cnt, this->grid_touched_cnt(), sizeof(uint32_t), cudaMemcpyDeviceToHost));
