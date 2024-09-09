0e6707858f701fb2d9aa224b47bfb19e749439cc
g1n0st <g1n0st@live.com>
2024-09-09 00:07:35 -0700

    [update, wip] simplify code structure and setup bagging framework wip

---
+++ examples/multibody/deformable/mpm_bagging.cc
@@ -0,0 +1,96 @@
+#include "drake/examples/multibody/deformable/mpm_cloth_shared.h"
+
+DEFINE_bool(write_files, false, "Enable dumping MPM data to files.");
+DEFINE_double(simulation_time, 25.0, "Desired duration of the simulation [s].");
+DEFINE_int32(res, 50, "Cloth Resolution.");
+DEFINE_double(realtime_rate, 1.0, "Desired real time rate.");
+DEFINE_double(time_step, 2e-3,
+              "Discrete time step for the system [s]. Must be positive.");
+DEFINE_double(substep, 5e-4,
+              "Discrete time step for the substepping scheme [s]. Must be positive.");
+DEFINE_string(contact_approximation, "sap",
+              "Type of convex contact approximation. See "
+              "multibody::DiscreteContactApproximation for details. Options "
+              "are: 'sap', 'lagged', and 'similar'.");
+DEFINE_double(stiffness, 1.5e3, "Contact Stiffness.");
+DEFINE_double(friction, 1.0, "Contact Friction.");
+DEFINE_double(damping, 1e-5,
+    "Hunt and Crossley damping for the deformable body, only used when "
+    "'contact_approximation' is set to 'lagged' or 'similar' [s/m].");
+
+namespace drake {
+namespace examples {
+namespace {
+
+int do_main() {
+  systems::DiagramBuilder<double> builder;
+
+  MultibodyPlantConfig plant_config;
+  plant_config.time_step = FLAGS_time_step;
+  plant_config.discrete_contact_approximation = FLAGS_contact_approximation;
+
+  auto [plant, scene_graph] = AddMultibodyPlant(plant_config, &builder);
+
+  ProximityProperties rigid_proximity_props;
+  ProximityProperties ground_proximity_props;
+  const CoulombFriction<double> surface_friction(1.0, 1.0);
+  AddCompliantHydroelasticProperties(1.0, 2e5, &rigid_proximity_props);
+  AddRigidHydroelasticProperties(1.0, &ground_proximity_props);
+  AddContactMaterial({}, {}, surface_friction, &rigid_proximity_props);
+  AddContactMaterial({}, {}, surface_friction, &ground_proximity_props);
+  IllustrationProperties illustration_props;
+  illustration_props.AddProperty("phong", "diffuse", Vector4d(0.7, 0.5, 0.4, 0.8));
+
+  /* Set up a ground. */
+  Box ground{4, 4, 4};
+  const RigidTransformd X_WG(Eigen::Vector3d{0, 0, -2 + 0.1});
+  // plant.RegisterCollisionGeometry(plant.world_body(), X_WG, ground, "ground_collision", ground_proximity_props);
+  plant.RegisterVisualGeometry(plant.world_body(), X_WG, ground, "ground_visual", std::move(illustration_props));
+
+  DeformableModel<double>& deformable_model = plant.mutable_deformable_model();
+  AddCloth(&deformable_model, FLAGS_res, 0.75);
+  // AddClothFromFile(&deformable_model, "/home/changyu/Desktop/tshirt.obj");
+
+  MpmConfigParams mpm_config;
+  mpm_config.substep_dt = FLAGS_substep;
+  mpm_config.write_files = FLAGS_write_files;
+  mpm_config.contact_stiffness = FLAGS_stiffness;
+  mpm_config.contact_damping = FLAGS_damping;
+  mpm_config.contact_friction_mu = FLAGS_friction;
+  mpm_config.contact_query_frequency = 8;
+  deformable_model.SetMpmConfig(std::move(mpm_config));
+
+  /* All rigid and deformable models have been added. Finalize the plant. */
+  plant.Finalize();
+
+  /* Add a visualizer that emits LCM messages for visualization. */
+  geometry::DrakeVisualizerParams visualize_params;
+  visualize_params.show_mpm = true;
+  auto& visualizer = geometry::DrakeVisualizerd::AddToBuilder(&builder, scene_graph, nullptr, visualize_params);
+
+  // NOTE (changyu): MPM shortcut port shuould be explicit connected for visualization.
+  builder.Connect(plant.get_output_port(
+    plant.deformable_model().mpm_output_port_index()), 
+    visualizer.mpm_input_port());
+
+  auto diagram = builder.Build();
+  std::unique_ptr<Context<double>> diagram_context = diagram->CreateDefaultContext();
+
+  /* Build the simulator and run! */
+  systems::Simulator<double> simulator(*diagram, std::move(diagram_context));
+  simulator.Initialize();
+  simulator.set_target_realtime_rate(FLAGS_realtime_rate);
+  simulator.AdvanceTo(FLAGS_simulation_time);
+
+  return 0;
+}
+
+}  // namespace
+}  // namespace examples
+}  // namespace drake
+
+int main(int argc, char* argv[]) {
+  gflags::SetUsageMessage("This is a demo used to showcase bagging simulations in Drake.");
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  return drake::examples::do_main();
+}
--- examples/multibody/deformable/mpm_tshirt_folding.cc
+++ examples/multibody/deformable/mpm_tshirt_folding.cc
@@ -1,26 +1,4 @@
-#include <memory>
-
-#include <gflags/gflags.h>
-
-#include "drake/common/find_resource.h"
-#include "drake/examples/multibody/deformable/parallel_gripper_controller.h"
-#include "drake/examples/multibody/deformable/point_source_force_field.h"
-#include "drake/examples/multibody/deformable/suction_cup_controller.h"
-#include "drake/geometry/drake_visualizer.h"
-#include "drake/geometry/proximity_properties.h"
-#include "drake/geometry/scene_graph.h"
-#include "drake/math/rigid_transform.h"
-#include "drake/multibody/fem/deformable_body_config.h"
-#include "drake/multibody/parsing/parser.h"
-#include "drake/multibody/plant/deformable_model.h"
-#include "drake/multibody/plant/multibody_plant.h"
-#include "drake/multibody/plant/multibody_plant_config_functions.h"
-#include "drake/multibody/tree/prismatic_joint.h"
-#include "drake/systems/analysis/simulator.h"
-#include "drake/systems/framework/diagram.h"
-#include "drake/systems/framework/diagram_builder.h"
-#include "drake/geometry/proximity/obj_to_surface_mesh.h"
-#include "drake/geometry/proximity/triangle_surface_mesh.h"
+#include "drake/examples/multibody/deformable/mpm_cloth_shared.h"
 
 DEFINE_bool(write_files, false, "Enable dumping MPM data to files.");
 DEFINE_double(simulation_time, 25.0, "Desired duration of the simulation [s].");
@@ -39,38 +17,6 @@ DEFINE_double(friction, 1.0, "Contact Friction.");
 DEFINE_double(damping, 1e-5,
     "Hunt and Crossley damping for the deformable body, only used when "
     "'contact_approximation' is set to 'lagged' or 'similar' [s/m].");
-
-using drake::geometry::ReadObjToTriangleSurfaceMesh;
-using drake::geometry::TriangleSurfaceMesh;
-using drake::examples::deformable::ParallelGripperController;
-using drake::geometry::AddContactMaterial;
-using drake::geometry::Box;
-using drake::geometry::Sphere;
-using drake::geometry::Capsule;
-using drake::geometry::GeometryInstance;
-using drake::geometry::IllustrationProperties;
-using drake::geometry::Mesh;
-using drake::geometry::ProximityProperties;
-using drake::math::RigidTransformd;
-using drake::multibody::AddMultibodyPlant;
-using drake::multibody::CoulombFriction;
-using drake::multibody::DeformableBodyId;
-using drake::multibody::DeformableModel;
-using drake::multibody::gmpm::MpmConfigParams;
-using drake::multibody::ModelInstanceIndex;
-using drake::multibody::MultibodyPlant;
-using drake::multibody::MultibodyPlantConfig;
-using drake::multibody::Parser;
-using drake::multibody::PrismaticJoint;
-using drake::multibody::RigidBody;
-using drake::multibody::SpatialInertia;
-using drake::multibody::fem::DeformableBodyConfig;
-using drake::systems::BasicVector;
-using drake::systems::Context;
-using Eigen::Vector2d;
-using Eigen::Vector3d;
-using Eigen::Vector4d;
-using Eigen::VectorXd;
 
 namespace drake {
 namespace examples {
@@ -244,62 +190,6 @@ class FoldingGripperController : public systems::LeafSystem<double> {
   }
 };
 
-[[maybe_unused]] void AddCloth(DeformableModel<double> *deformable_model, int res, double z_axis) {
-  const double l = 0.007 * res;
-  int length = res;
-  int width = res;
-  double dx = l / width;
-
-  auto p = [&](int i, int j) {
-    return i * width + j;
-  };
-
-  {
-    std::vector<Eigen::Vector3d> inital_pos;
-    std::vector<Eigen::Vector3d> inital_vel;
-    std::vector<int> indices;
-    for (int i = 0; i < length; ++i) {
-      for (int j = 0; j < width; ++j) {
-        inital_pos.emplace_back((0.5 - 0.5 * l) + i * dx, (0.5 - 0.5 * l) + j * dx, z_axis);
-        inital_vel.emplace_back(0., 0., 0.);
-      }
-    }
-
-    for (int i = 0; i < length; ++i) {
-      for (int j = 0; j < width; ++j) {
-        if (i < length - 1 && j < width - 1) {
-          indices.push_back(p(i, j));
-          indices.push_back(p(i+1, j));
-          indices.push_back(p(i, j+1));
-
-          indices.push_back(p(i+1, j+1));
-          indices.push_back(p(i, j+1));
-          indices.push_back(p(i+1, j));
-        }
-      }
-    }
-
-    deformable_model->RegisterMpmCloth(inital_pos, inital_vel, indices);
-  }
-}
-
-[[maybe_unused]] void AddClothFromFile(DeformableModel<double> *deformable_model, std::string filename) {
-  const auto &mesh = ReadObjToTriangleSurfaceMesh(filename);
-  std::vector<Eigen::Vector3d> inital_pos;
-  std::vector<Eigen::Vector3d> inital_vel;
-  std::vector<int> indices;
-  for (int i = 0; i < mesh.num_triangles(); ++i) {
-    indices.push_back(mesh.triangles()[i].vertex(0));
-    indices.push_back(mesh.triangles()[i].vertex(1));
-    indices.push_back(mesh.triangles()[i].vertex(2));
-  }
-  for (int i = 0; i < mesh.num_vertices(); ++i) {
-    inital_pos.emplace_back(mesh.vertices()[i][0], mesh.vertices()[i][2], mesh.vertices()[i][1]); // swap y-axis and z-axis
-    inital_vel.emplace_back(0., 0., 0.);
-  }
-  deformable_model->RegisterMpmCloth(inital_pos, inital_vel, indices);
-}
-
 int do_main() {
   systems::DiagramBuilder<double> builder;
 
@@ -372,7 +262,7 @@ int do_main() {
 }  // namespace drake
 
 int main(int argc, char* argv[]) {
-  gflags::SetUsageMessage("This is a demo used to showcase cloth folding simulations in Drake.");
+  gflags::SetUsageMessage("This is a demo used to showcase tshirt folding simulations in Drake.");
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   return drake::examples::do_main();
 }
