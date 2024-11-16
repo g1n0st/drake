311ad68c6279704e2b36078d0d560e527ad1c236
g1n0st <g1n0st@live.com>
2024-11-15 18:56:16 -0800

    [fix] mass only as the weighting

--- multibody/gpu_mpm/cuda_mpm_kernels.cuh
+++ multibody/gpu_mpm/cuda_mpm_kernels.cuh
@@ -1155,7 +1155,7 @@ __global__ void contact_particle_to_grid_kernel(const size_t n_particles,
 #endif
         T weight = weights[threadIdx.x][i][0] * weights[threadIdx.x][j][1] * weights[threadIdx.x][k][2];
         #pragma unroll
-        for (int ii = 0; ii < 9; ++ii) val[ii] *= mass * weight * weight * mass;
+        for (int ii = 0; ii < 9; ++ii) val[ii] *= mass * weight * weight;
         for (int ii = 9; ii < 12; ++ii) val[ii] *= mass * weight;
 
         for (int iter = 1; iter <= mark; iter <<= 1) {
@@ -1382,9 +1382,9 @@ __global__ void grid_to_particle_vdb_line_search_kernel(const size_t n_particles
         };
 
         T weight = weights[threadIdx.x][ii][0] * weights[threadIdx.x][jj][1] * weights[threadIdx.x][kk][2];
-        atomicAdd(&g_E1[color_index], l(new_v_local));
+        atomicAdd(&g_E1[color_index], mass * l(new_v_local));
         if constexpr(EVAL_E0) {
-            atomicAdd(&g_E0[color_index], l(old_v_local));
+            atomicAdd(&g_E0[color_index], mass * l(old_v_local));
         }
     }
 }
