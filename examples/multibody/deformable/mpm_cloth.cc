2e4338894473b67dfd7e31e7d7427fee36ba3f95
Chang Yu <g1n0st@live.com>
2025-01-26 16:27:47 -0800

    [update, add] implement margin for mpm and rigid

--- examples/multibody/deformable/mpm_cloth.cc
+++ examples/multibody/deformable/mpm_cloth.cc
@@ -55,7 +55,8 @@ DEFINE_string(contact_approximation, "sap",
 // NOTE (changyu): here we choose k=100 for smaller amount of penetration (0.01mm or 1e-5m).
 DEFINE_double(stiffness, 10.0, "Contact Stiffness.");
 DEFINE_double(friction, 0.0, "Contact Friction.");
-DEFINE_double(damping, 1e-5,
+DEFINE_double(margin, 1.0, "MPM-Rigid Margin.");
+DEFINE_double(damping, 1e-2,
     "Hunt and Crossley damping for the deformable body, only used when "
     "'contact_approximation' is set to 'lagged' or 'similar' [s/m].");
 DEFINE_bool(exact_line_search, false, "Enable exact_line_search for contact solving.");
@@ -239,6 +240,7 @@ int do_main() {
   mpm_config.contact_damping = FLAGS_damping;
   mpm_config.contact_friction_mu = FLAGS_friction;
   mpm_config.exact_line_search = FLAGS_exact_line_search;
+  mpm_config.margin = FLAGS_margin;
   deformable_model.SetMpmConfig(std::move(mpm_config));
 
   /* All rigid and deformable models have been added. Finalize the plant. */
