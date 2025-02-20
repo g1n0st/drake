ce6d6be7067ce00f157990e89182f5198e4cc38a
Chang Yu <g1n0st@live.com>
2025-02-20 14:47:54 -0800

    [update] enable exact line search

--- examples/multibody/deformable/allegro_bagging.cc
+++ examples/multibody/deformable/allegro_bagging.cc
@@ -628,6 +628,8 @@ int do_main() {
   mpm_config.contact_damping = FLAGS_damping;
   mpm_config.contact_friction_mu = FLAGS_friction;
   mpm_config.contact_query_frequency = 8;
+  mpm_config.exact_line_search = true;
+  mpm_config.mdv_as_impulse = false;
   if (use_mpm_gripper) {
     mpm_config.mpm_bc = 114;
   } else {
