2df185f2630e8b53b230a07230a1bf54bf320f65
Chang Yu <g1n0st@live.com>
2025-11-04 12:18:19 +0000

    [fix] use 5s for simulation time

--- examples/multibody/rolling_sphere_mpm/rolling_sphere_mpm.cc
+++ examples/multibody/rolling_sphere_mpm/rolling_sphere_mpm.cc
@@ -23,7 +23,7 @@
 #include "drake/systems/framework/diagram.h"
 #include "drake/systems/framework/diagram_builder.h"
 
-DEFINE_double(simulation_time, 2.4, "Desired duration of the simulation [s].");
+DEFINE_double(simulation_time, 5.0, "Desired duration of the simulation [s].");
 DEFINE_double(realtime_rate, 1.0, "Desired real time rate.");
 DEFINE_double(time_step, 1e-2,
               "Discrete time step for the system [s]. Must be positive.");
